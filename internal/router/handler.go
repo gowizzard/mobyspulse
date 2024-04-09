b13d6bd5921f88c65abfdbbaf0888b0561b6f1a6
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2024-04-09 10:01:03 +0200

    feat: Add handler function to repository.

---
+++ internal/router/handler.go
@@ -0,0 +1,15 @@
+// Package router is to define the different router for the http server.
+package router
+
+import (
+	"github.com/gowizzard/mobyspulse/internal/handler"
+	"github.com/gowizzard/mobyspulse/internal/redirect"
+	"net/http"
+)
+
+// Handler are to register the different handlers.
+func Handler(mux *http.ServeMux) {
+	mux.HandleFunc(http.MethodGet+" /", redirect.Metrics)
+	mux.HandleFunc(http.MethodGet+" /favicon.ico", handler.Favicon)
+	mux.HandleFunc(http.MethodGet+" /metrics", handler.Metrics)
+}
